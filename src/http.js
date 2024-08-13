ce872cab04b391dc13a824ac845554f8b71cad08
Brandon Jordan <brandonjordan124@gmail.com>
2024-08-12 21:03:25 -0400

    Organize http

--- src/http.js
+++ src/http.js
@@ -6,6 +6,164 @@ import {empty} from './util.js';
 import {Store} from './store.js';
 
 const methods = ['get', 'post', 'put', 'patch', 'delete'];
+const defaultFetchableConfig = {
+    headers: new Headers(),
+};
+
+export class Fetchable {
+    busy = new Store(false);
+    url;
+    config;
+    response;
+
+    constructor(url, config = {}) {
+        this.url = url;
+        this.config = {...defaultFetchableConfig, ...config};
+    }
+
+    async text() {
+        return await this.get(res => res.text());
+    }
+
+    async json() {
+        if (this.config.hasOwnProperty('headers')) {
+            if (!this.config.headers.has('Accept') || this.config.headers.get('Accept') !== 'application/json') {
+                this.config.headers.set('Accept', 'application/json');
+            }
+        } else {
+            this.config.headers = new Headers({'Accept': 'application/json'});
+        }
+
+        return await this.get(res => res.json());
+    }
+
+    async formData() {
+        return await this.get(res => res.formData());
+    }
+
+    async blob() {
+        return await this.get(res => res.blob());
+    }
+
+    async arrayBuffer() {
+        return await this.get(res => res.arrayBuffer());
+    }
+
+    async get(returnTypeFn) {
+        this.busy.set(true);
+        return await fetch(this.url, this.config).then(res => {
+            this.response = res;
+            return returnTypeFn(res);
+        }).finally(() => this.busy.set(false));
+    }
+}
+
+// Store for JSON data from a single API endpoint. Use fetch() method to refresh data for subscribers.
+export class APIStore extends Store {
+    url;
+    busy = new Store(false);
+
+    constructor(url = null) {
+        super({});
+        if (url) {
+            this.url = url;
+        }
+        this.fetch();
+    }
+
+    async fetch() {
+        this.busy.set(true);
+        await fetch(this.url).then(res => res.json()).then(data => this.set(data)).catch(this.onError).finally(() => this.busy.set(false));
+    }
+
+    onError(err) {
+        //
+    }
+}
+
+export class HTTPStore extends Store {
+    url;
+    request;
+    busy = new Store(false);
+    errorHandler;
+
+    constructor(method, url, options = {}) {
+        super();
+
+        if (!methods.includes(method.toLowerCase())) {
+            console.warn(`[WARN] Unknown HTTP method ${method.toUpperCase()}`);
+        }
+
+        this.url = url;
+        this.request = new XMLHttpRequest();
+
+        if (!empty(options)) {
+            if (options.responseType) {
+                this.request.responseType = options.responseType;
+            }
+            if (options.data) {
+                if (this.method === 'GET') {
+                    url += '?' + new URLSearchParams(options.data).toString();
+                } else {
+                    let formData = new FormData();
+                    for (let key in options.data) {
+                        // @ts-ignore
+                        formData.append(key, options.data[key]);
+                    }
+                    this.body = formData;
+                }
+            }
+            if (options.headers) {
+                for (let header in options.headers) {
+                    // @ts-ignore
+                    this.request.setRequestHeader(header, options.headers[header]);
+                }
+            }
+        }
+        this.request.open(method, url, true);
+    }
+
+    async fetch() {
+        this.busy.set(true);
+        this.request.send(this.body);
+        return await new Promise(resolve => {
+            this.request.onload = () => {
+                if (this.request.status >= 200 && this.request.status < 400) {
+                    let response = {
+                        data: this.request.response,
+                        type: this.request.responseType,
+                        status: this.request.status,
+                        statusText: this.request.statusText,
+                    };
+                    this.set(response);
+                    resolve();
+                } else {
+                    this.error();
+                    throw new Error(this.request.response);
+                }
+            };
+            this.request.onerror = (err) => this.errorHandler(err, 'error');
+            this.request.ontimeout = (err) => this.errorHandler(err, 'timeout');
+            this.request.onabort = (err) => this.errorHandler(err, 'abort');
+        }).finally(() => this.busy.set(false));
+    }
+
+    onProgress(handler) {
+        this.request.onprogress = (oEvent) => {
+            if (oEvent.lengthComputable) {
+                handler(oEvent.loaded / oEvent.total * 100);
+            } else {
+                // Unable to compute progress information since the total size is unknown
+                handler(0);
+            }
+        };
+        return this;
+    }
+
+    onError(handler) {
+        this.errorHandler = handler;
+    }
+}
 
 export class APIModel {
     id;
@@ -178,164 +336,5 @@ export class APIModel {
                 throw new Error(`[${this.constructor.name} (${id})] Field '${key}' is not fillable!`);
             }
         }
-    }
-}
-
-// Store for JSON data from a single API endpoint. Use fetch() method to refresh data for subscribers.
-export class APIStore extends Store {
-    url;
-    busy = new Store(false);
-
-    constructor(url = null) {
-        super({});
-        if (url) {
-            this.url = url;
-        }
-        this.fetch();
-    }
-
-    async fetch() {
-        this.busy.set(true);
-        await fetch(this.url).then(res => res.json()).then(data => this.set(data)).catch(this.onError).finally(() => this.busy.set(false));
-    }
-
-    onError(err) {
-        //
-    }
-}
-
-export class HTTPStore extends Store {
-    url;
-    request;
-    busy = new Store(false);
-    errorHandler;
-
-    constructor(method, url, options = {}) {
-        super();
-
-        if (!methods.includes(method.toLowerCase())) {
-            console.warn(`[WARN] Unknown HTTP method ${method.toUpperCase()}`);
-        }
-
-        this.url = url;
-        this.request = new XMLHttpRequest();
-
-        if (!empty(options)) {
-            if (options.responseType) {
-                this.request.responseType = options.responseType;
-            }
-            if (options.data) {
-                if (this.method === 'GET') {
-                    url += '?' + new URLSearchParams(options.data).toString();
-                } else {
-                    let formData = new FormData();
-                    for (let key in options.data) {
-                        // @ts-ignore
-                        formData.append(key, options.data[key]);
-                    }
-                    this.body = formData;
-                }
-            }
-            if (options.headers) {
-                for (let header in options.headers) {
-                    // @ts-ignore
-                    this.request.setRequestHeader(header, options.headers[header]);
-                }
-            }
-        }
-        this.request.open(method, url, true);
-    }
-
-    async fetch() {
-        this.busy.set(true);
-        this.request.send(this.body);
-        return await new Promise(resolve => {
-            this.request.onload = () => {
-                if (this.request.status >= 200 && this.request.status < 400) {
-                    let response = {
-                        data: this.request.response,
-                        type: this.request.responseType,
-                        status: this.request.status,
-                        statusText: this.request.statusText,
-                    };
-                    this.set(response);
-                    resolve();
-                } else {
-                    this.error();
-                    throw new Error(this.request.response);
-                }
-            };
-            this.request.onerror = (err) => this.errorHandler(err, 'error');
-            this.request.ontimeout = (err) => this.errorHandler(err, 'timeout');
-            this.request.onabort = (err) => this.errorHandler(err, 'abort');
-        }).finally(() => this.busy.set(false));
-    }
-
-    onProgress(handler) {
-        this.request.onprogress = (oEvent) => {
-            if (oEvent.lengthComputable) {
-                handler(oEvent.loaded / oEvent.total * 100);
-            } else {
-                // Unable to compute progress information since the total size is unknown
-                handler(0);
-            }
-        };
-        return this;
-    }
-
-    onError(handler) {
-        this.errorHandler = handler;
-    }
-}
-
-const defaultFetchableConfig = {
-    headers: new Headers(),
-};
-
-export class Fetchable {
-    busy = new Store(false);
-    url;
-    config;
-    response;
-
-    constructor(url, config = {}) {
-        this.url = url;
-        this.config = {...defaultFetchableConfig, ...config};
-    }
-
-    async text() {
-        return await this.get(res => res.text());
-    }
-
-    async json() {
-        if (this.config.hasOwnProperty('headers')) {
-            if (!this.config.headers.has('Accept') || this.config.headers.get('Accept') !== 'application/json') {
-                this.config.headers.set('Accept', 'application/json');
-            }
-        } else {
-            this.config.headers = new Headers({'Accept': 'application/json'});
-        }
-
-        return await this.get(res => res.json());
-    }
-
-    async formData() {
-        return await this.get(res => res.formData());
-    }
-
-    async blob() {
-        return await this.get(res => res.blob());
-    }
-
-    async arrayBuffer() {
-        return await this.get(res => res.arrayBuffer());
-    }
-
-    async get(returnTypeFn) {
-        this.busy.set(true);
-        return await fetch(this.url, this.config).then(res => {
-            this.response = res;
-            return returnTypeFn(res);
-        }).finally(() => this.busy.set(false));
     }
 }
