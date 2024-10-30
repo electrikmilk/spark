f7f61c112d8a7f54f2bba6f5abaa783fac76da8c
Brandon Jordan <brandonjordan124@gmail.com>
2024-10-29 20:21:17 -0400

    Refactor onFetch()

--- src/http.js
+++ src/http.js
@@ -22,11 +22,11 @@ export class FetchStore extends Store {
 
     async fetch() {
         this.busy.set(true);
-        await fetch(this.url).then(res => res.json()).then(data => this.onFetch(data)).catch(this.onError).finally(() => this.busy.set(false));
+        await fetch(this.url).then(res => res.json()).then(data => this.set(this.onFetch(data))).catch(this.onError).finally(() => this.busy.set(false));
     }
 
     onFetch(data) {
-        this.set(data);
+        return data;
     }
 
     onError(err) {
