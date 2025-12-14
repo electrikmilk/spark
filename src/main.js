dcfbb9055dffb71fa9327d93aafdbe4480de3da5
Brandon Jordan <brandonjordan124@gmail.com>
2025-12-14 17:26:42 -0500

    Fix exports

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
  * Spark library.
  */
 
-export * from './html.js';
-export * from './store.js';
-export * from './resources.js';
-export * from './url.js';
-export * from './random.js';
-export * from './local-storage.js';
-export * from './refs.js';
 export * from './animation.js';
-export * from './style.js';
+export * from './custom-elements.js';
+export * from './html.js';
 export * from './http.js';
+export * from './local-storage.js';
+export * from './random.js';
+export * from './refs.js';
+export * from './resources.js';
+export * from './store.js';
+export * from './style.js';
+export * from './url.js';
 export * from './util.js';
+export * from './view.js';
